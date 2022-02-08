fc55d1890da4cf0a8779b28151d3b09519bcc150
fjetter <fjetter@users.noreply.github.com>
2022-02-08 18:38:11 +0100

    Drop Cube support

--- kartothek/api/cube.py
+++
@@ -1,4 +0,0 @@
-from kartothek.core.cube import *  # noqa: F401, F403
-from kartothek.io.dask.bag_cube import *  # noqa: F401, F403
-from kartothek.io.dask.dataframe_cube import *  # noqa: F401, F403
-from kartothek.io.eager_cube import *  # noqa: F401, F403
