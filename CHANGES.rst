bdaebdf255bc0a7b467eaa504d2aed49876bc814
Uwe L. Korn <uwe.korn@quantco.com>
2022-10-12 10:48:22 +0200

    Test for pyarrow 5
    Fixes #10

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,7 @@ Changelog
 Plateau 4.1.0 (2022-09-??)
 ==========================
 
-* Support for ``pyarrow`` 6, 7, 8, 9
+* Support for ``pyarrow`` 5, 6, 7, 8, 9
 * Support for ``numpy`` 1.23
 * Support for ``dask>=2022.4.2``
 * Support Python 3.8-3.10
--- plateau/serialization/_parquet.py
+++ plateau/serialization/_parquet.py
@@ -42,12 +42,12 @@ _logger = logging.getLogger(__name__)
 EPOCH_ORDINAL = datetime.date(1970, 1, 1).toordinal()
 MAX_NB_RETRIES = 6  # longest retry backoff = BACKOFF_TIME * 2**(MAX_NB_RETRIES - 2)
 BACKOFF_TIME = 0.01  # 10 ms
-PYARROW_LT_5 = version.parse(pa.__version__) < version.parse("5")
+PYARROW_LT_6 = version.parse(pa.__version__) < version.parse("6")
 PYARROW_LT_8 = version.parse(pa.__version__) < version.parse("8")
 
-# Since pyarrow 5, the Parquet version/features can be selected more granular.
+# Since pyarrow 6, the Parquet version/features can be selected more granular.
 # Version 2.0 is equal to 2.4 but 2.4 doesn't trigger deprecation warnings.
-if PYARROW_LT_5:
+if PYARROW_LT_6:
     PARQUET_VERSION = "2.0"
 else:
     PARQUET_VERSION = "2.4"
