bf471869796f72e50a7eecfe7d9508928e69aef3
Izer Onadim <143251429+IzerOnadimQC@users.noreply.github.com>
2023-10-30 17:25:55 +0000

    Remove pandas<2.1.0 pin (#106)
    * Removes pandas less-than 2.1.0 pin
    * Coerces timestamps to ns in index column reconstruction
    * Replaces scipy with scientific-python for nightly install
    
    Co-authored-by: Jan Tilly <jantilly@gmail.com>

--- tests/io_components/test_dataset.py
+++ tests/io_components/test_dataset.py
@@ -4,7 +4,6 @@ from collections import OrderedDict
 
 import pandas as pd
 import pandas.testing as pdt
-from packaging.version import Version
 
 from plateau.core.dataset import DatasetMetadata
 from plateau.core.index import ExplicitSecondaryIndex
@@ -103,10 +102,7 @@ def test_dataset_get_indices_as_dataframe_predicates():
         columns=["l_external_code"],
         predicates=[[("l_external_code", "==", "1"), ("p_external_code", "==", "3")]],
     )
-    if Version(pd.__version__) < Version("2.0.0.dev0"):
-        empty_index = pd.Index([], name="partition")
-    else:
-        empty_index = pd.Index([], name="partition", dtype="int64")
+    empty_index = pd.Index([], name="partition")
     expected = pd.DataFrame(columns=["l_external_code"], index=empty_index)
     pdt.assert_frame_equal(result, expected)
 
@@ -120,10 +116,7 @@ def test_dataset_get_indices_as_dataframe_no_index(dataset):
 def test_dataset_get_indices_as_dataframe_with_index(dataset_with_index, store_session):
     assert not dataset_with_index.primary_indices_loaded
     df = dataset_with_index.get_indices_as_dataframe()
-    if Version(pd.__version__) < Version("2.0.0.dev0"):
-        empty_index = pd.Index([], name="partition")
-    else:
-        empty_index = pd.Index([], name="partition", dtype="int64")
+    empty_index = pd.Index([], name="partition")
     pdt.assert_frame_equal(
         df,
         pd.DataFrame(columns=["L", "P"], index=empty_index).astype(
